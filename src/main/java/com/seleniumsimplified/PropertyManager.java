4be8e493bb5166dc040c0bd8b2e91e20dfd2bcdf
gonzalezrodero <gonzalez.rodero@gmail.com>
2017-06-20 09:17:11 +0100

    changes in tests to pass in mac

--- src/main/java/com/seleniumsimplified/PropertyManager.java
+++ src/main/java/com/seleniumsimplified/PropertyManager.java
@@ -7,9 +7,11 @@ public class PropertyManager {
     private static Properties properties;
 
     public static String getProperty(String propertyKey) throws IOException {
-        FileInputStream propertiesFileInput = new FileInputStream(new File("src/main/resources/config.properties.xml"));
+        //FileInputStream propertiesFileInput = new FileInputStream(new File("src/main/resources/config.properties.xml"));
+        FileInputStream propertiesFileInput = new FileInputStream(new File("src/main/resources/config.properties"));
         properties = new Properties();
-        properties.loadFromXML(propertiesFileInput);
+        //properties.loadFromXML(propertiesFileInput);
+        properties.load(propertiesFileInput);
         propertiesFileInput.close();
         return properties.getProperty(propertyKey);
     }
